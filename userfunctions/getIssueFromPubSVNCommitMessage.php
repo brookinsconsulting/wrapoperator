b75d4711a6220c4c5595a5ae50bddc02d18f0610
Brookins Consulting <info@brookinsconsulting.com>
2009-08-21 04:41:48 +0000

    Updated: Refined function with the addition of conditional logic tests.

--- userfunctions/getIssueFromPubSVNCommitMessage.php
+++ userfunctions/getIssueFromPubSVNCommitMessage.php
@@ -5,13 +5,17 @@
     {
         //given string $data, will return the first $issue string in that string
 	$ret = false;
-	$split = split( "#", $data );
-	$match = $split[1];
-	$issue = substr( $match, 0, +5 );
 
-	if ( $issue != '' && count( $issue ) <= 5 ) {
-            // eZDebug::writeDebug( "wrap_operator: getIssueFromPubSVNCommitMessage, results: " . print_r( $issue, TRUE) );
- 	    $ret = $issue;
+	$split = split( "#", $data );
+
+	if( isset( $split[1] ) ) {
+	    $match = $split[1];
+    	    $issue = substr( $match, 0, +5 );
+
+	    if ( $issue != '' && count( $issue ) <= 5 ) {
+               // eZDebug::writeDebug( "wrap_operator: getIssueFromPubSVNCommitMessage, results: " . print_r( $issue, TRUE) );
+ 	       $ret = $issue;
+	    }
 	}
 
         return $ret;
