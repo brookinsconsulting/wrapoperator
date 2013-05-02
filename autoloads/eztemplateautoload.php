12ed5cb3d6d41de9391df89c8401542feeec7e47
Brookins Consulting <info@brookinsconsulting.com>
2013-05-01 19:20:30 -0500

    Updated: Minor improvements to template autoloads whitespace formatting

--- autoloads/eztemplateautoload.php
+++ autoloads/eztemplateautoload.php
@@ -5,4 +5,5 @@ $eZTemplateOperatorArray = array();
 $eZTemplateOperatorArray[] = array( 'script' => 'extension/wrapoperator/autoloads/WrapOperator.php',
                                     'class' => 'WrapOperator',
                                     'operator_names' => array( 'wrap_php_func', 'wrap_user_func' ) );
+
 ?>
